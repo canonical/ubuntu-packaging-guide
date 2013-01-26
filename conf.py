27cdb1322308e229619f6566944cee7af425c5e5
Andrew Starr-Bochicchio <a.starr.b@gmail.com>
2013-01-26 16:46:46 -0500

    Add a circle of friends Ubuntu logo to pdf title page.

--- conf.py
+++ conf.py
@@ -196,7 +196,7 @@ latex_documents = [
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
-#latex_logo = None
+latex_logo = 'images/logo-ubuntu_cof-orange-hex.png'
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
