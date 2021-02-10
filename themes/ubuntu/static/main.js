e1d95a3a072de111269928c63efc19c35325590e
Dmitry Shachnev <mitya57@ubuntu.com>
2021-02-10 21:04:47 +0300

    Add quotes to fix error from jQuery: unrecognized expression: a[href=#top]

--- themes/ubuntu/static/main.js
+++ themes/ubuntu/static/main.js
@@ -14,7 +14,7 @@ $(function() {
         }
     });
 
-    $('a[href=#top]').click(function(){
+    $('a[href="#top"]').click(function(){
         $('html, body').animate({scrollTop:0}, 'slow');
         return false;
     });
