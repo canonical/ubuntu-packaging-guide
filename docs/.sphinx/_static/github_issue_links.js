a601503ad02e10d2aaa6eee572e8d5401ca63712
Robert Krátký <rkratky@gmail.com>
2023-11-23 16:19:24 +0100

    Adopt Starter Pack config.

--- docs/.sphinx/_static/github_issue_links.js
+++ docs/.sphinx/_static/github_issue_links.js
@@ -1,10 +1,19 @@
+// if we already have an onload function, save that one
+var prev_handler = window.onload;
+
 window.onload = function() {
+    // call the previous onload function
+    if (prev_handler) {
+        prev_handler();
+    }
+
     const link = document.createElement("a");
     link.classList.add("muted-link");
     link.classList.add("github-issue-link");
     link.text = "Give feedback";
     link.href = (
-        "https://github.com/canonical/ubuntu-packaging-guide/issues/new?"
+        github_url
+        + "/issues/new?"
         + "title=docs%3A+TYPE+YOUR+QUESTION+HERE"
         + "&body=*Please describe the question or issue you're facing with "
         + `"${document.title}"`
